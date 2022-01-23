16970cb569b8a90a7df1b83acb7b21d29b149472
toomspawt <anhvnguyen22@gmail.com>
2022-01-23 15:32:45 -0500

    Add email to user creation

--- myproject/accounts/tests/test_view_signup.py
+++ myproject/accounts/tests/test_view_signup.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .views import signup
+from ..views import signup
+from ..forms import SignUpForm
 
 
 class SignUpTest(TestCase):
@@ -22,7 +23,19 @@ class SignUpTest(TestCase):
 
     def test_contain_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        This test is to make sure nobody change the form
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
 
 
 
@@ -31,6 +44,7 @@ class SuccessfulSignUpTest(TestCase):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'john@test.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456',
         }
