86165d2e0c834d360902d51ed6cfb319f2230aa9
toomspawt <anhvnguyen22@gmail.com>
2022-01-23 22:44:57 -0500

    Add user login

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -132,3 +132,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'home'
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('board/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
     path('signup/', accounts_views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
