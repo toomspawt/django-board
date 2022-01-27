224de79ccad0493d78647a44712030ed0805d3a2
toomspawt <anhvnguyen22@gmail.com>
2022-01-26 20:26:24 -0500

    Add displaying topics and replies count

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -8,11 +9,20 @@ class Board(models.Model):
     def __str__(self):
         return self.name
 
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_update = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -21,4 +31,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
-    
+
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
